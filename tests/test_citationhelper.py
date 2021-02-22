35db4e02f0bb1d0964fdee243a01b14d288b177e
Leslie Lamarche <leslie.lamarche@sri.com>
2021-02-22 09:10:05 -0800

    Rename test script so pytest finds it and change hardcoded paths.

--- tests/test_citationhelper.py
+++ tests/test_citationhelper.py
@@ -4,11 +4,11 @@ import pytest
 import citationhelper.citationhelper as ch
 
 def test_py():
-    imports = ch.collect_imports(['py_scripts'])
+    imports = ch.collect_imports(['tests/py_scripts'])
     expected_imports = ['copy', 'custom_scripts', 'datetime', 'gzip', 'my_functions', 'my_utils', 'numpy', 'os', 'scipy', 'sys']
     assert imports == expected_imports
 
 def test_ipynb():
-    imports = ch.collect_imports(['ipynb_notebooks'])
+    imports = ch.collect_imports(['tests/ipynb_notebooks'])
     expected_imports = ['datetime', 'my_scripts', 'my_scripts2', 'numpy', 'os', 'scipy', 'sys']
     assert imports == expected_imports
