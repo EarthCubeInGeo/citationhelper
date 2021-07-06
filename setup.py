1d1fe2b4f5ea805fb4dcf1c1cc597175f9dbe4d0
Leslie Lamarche <leslie.lamarche@sri.com>
2021-07-06 08:29:20 -0700

    Roll version number in setup.py to 0.3.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ repo root directory."
 setup(name='citationhelper',
       install_requires=REQUIREMENTS,
       setup_requires=REQUIREMENTS,
-      version="0.2",
+      version="0.3",
       description=DESC,
       author="InGeO",
       author_email="ingeo-team@ingeo.datatransport.org",
