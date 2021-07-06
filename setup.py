4de34e2a591eb41259f1be62d4467d333e330de7
Leslie Lamarche <leslie.lamarche@sri.com>
2021-07-06 08:13:40 -0700

    Add pyproject.toml and modify README and setup.py such that the project builds for pypi.

--- setup.py
+++ setup.py
@@ -11,11 +11,12 @@ import sys
 import subprocess
 from setuptools import find_packages, setup
 
-# Get the package requirements
-REQSFILE = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-with open(REQSFILE, 'r') as f:
-    REQUIREMENTS = f.readlines()
-REQUIREMENTS = '\n'.join(REQUIREMENTS)
+# # Get the package requirements
+# REQSFILE = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+# with open(REQSFILE, 'r') as f:
+#     REQUIREMENTS = f.readlines()
+# REQUIREMENTS = '\n'.join(REQUIREMENTS)
+REQUIREMENTS = ''
 
 # # Do some nice things to help users install on conda.
 # if sys.version_info[:2] < (3, 0):
@@ -64,7 +65,7 @@ setup(name='citationhelper',
       long_description=READMETXT,
       zip_safe=False,
       py_modules=['citationhelper'],
-      classifiers=["Development Status :: 0.2 - Release",
+      classifiers=["Development Status :: 5 - Production/Stable",
                    "Topic :: Scientific/Engineering",
                    "Intended Audience :: Science/Research",
                    "License :: OSI Approved :: GNU General Public License (GPL)",
